d703a17fec33d4bc0ba84420881773d205e241ce
fmzu <u1900050@gmail.com>
2024-08-17 21:50:19 +0900

    update

--- api/route.ts
+++ api/route.ts
@@ -1,3 +1,9 @@
+import { authHandler, initAuthConfig } from "@hono/auth-js"
 import { Hono } from "hono"
+import { getAuthConfig } from "./get-auth-config"
+import { usersRoute } from "./routes/users"
 
-export const api = new Hono()
+export const api = new Hono<{ Bindings: { DB: D1Database } }>()
+  .use("*", initAuthConfig(getAuthConfig))
+  .use("/api/auth/*", authHandler())
+  .route("/api/users", usersRoute)
